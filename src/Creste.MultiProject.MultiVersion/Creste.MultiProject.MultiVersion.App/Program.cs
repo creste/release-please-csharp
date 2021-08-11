6b89f63ed9681d6546e49c8949be970049add322
creste <creste@users.noreply.github.com>
2021-08-11 14:38:07 -0400

    feat: added Creste.MultiProject.MultiVersion

---
+++ src/Creste.MultiProject.MultiVersion/Creste.MultiProject.MultiVersion.App/Program.cs
@@ -0,0 +1,13 @@
+﻿using Creste.MultiProject.MultiVersion.Lib;
+using System;
+
+namespace Creste.MultiProject.MultiVersion.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Messages.HelloWorld);
+        }
+    }
+}
---
+++ src/Creste.MultiProject.MultiVersion/Creste.MultiProject.MultiVersion.Lib/Messages.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Creste.MultiProject.MultiVersion.Lib
+{
+    public static class Messages
+    {
+        public static readonly string HelloWorld = "Hello World";
+    }
+}
