9fe0b6ccb149564339138264e04140bdbdfcc195
creste <creste@users.noreply.github.com>
2021-08-11 11:50:39 -0400

    feat: Add SingleProject solution

---
+++ src/Creste.SingleProject/Creste.SingleProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Creste.SingleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Single project");
+        }
+    }
+}
